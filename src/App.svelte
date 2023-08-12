347492ab65e6abd28c5a0321c95298ba25f320b2
orphandeity <88175879+orphandeity@users.noreply.github.com>
2023-08-12 00:20:14 -0700

    initial commit

---
+++ src/App.svelte
@@ -0,0 +1,29 @@
+<main>
+  <h1>Your Result</h1>
+  <div>76 of 100</div>
+
+  <h2>Great</h2>
+  <p>You scored higher than 65% of the people who have taken these tests.</p>
+  <div>
+    <h2>Summary</h2>
+    <ul>
+      <li>
+        <div>Reaction</div>
+        <div>80 / 100</div>
+      </li>
+      <li>
+        <div>Memory</div>
+        <div>92 / 100</div>
+      </li>
+      <li>
+        <div>Verbal</div>
+        <div>61 / 100</div>
+      </li>
+      <li>
+        <div>Visual</div>
+        <div>72 / 100</div>
+      </li>
+    </ul>
+    <button>Continue</button>
+  </div>
+</main>
